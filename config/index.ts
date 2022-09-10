e3c130ed20550f4fe6481710ba19b1f98dd0434d
Mahit Mehta <mahit.py@gmail.com>
2022-09-10 19:21:51 -0400

    V1.5 RC

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import development from "./env/development";
 import production from "./env/production";
 import extend from "lodash/extend";
 
-const dynamicConfig = !__DEV__ && process.env.APP_SERVER !== "production" ? development : production; 
+const dynamicConfig = __DEV__ && process.env.APP_SERVER !== "production" ? development : production; 
 
 const config = extend({
     name: "Gradebook"
