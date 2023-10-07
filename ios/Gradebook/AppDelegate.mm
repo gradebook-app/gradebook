e280db103c325ea2cc03c8aba7e611df0003f966
mahitmehta <mahit.py@gmail.com>
2023-10-06 23:58:50 -0400

    Removed expo-update

--- ios/Gradebook/AppDelegate.mm
+++ ios/Gradebook/AppDelegate.mm
@@ -8,16 +8,25 @@
   [FIRApp configure];
   
   self.moduleName = @"main";
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
   
+  [self.window makeKeyAndVisible];
+    
    UIStoryboard *sb = [UIStoryboard storyboardWithName:@"SplashScreen" bundle:nil];
    UIViewController *vc = [sb instantiateInitialViewController];
    
    ((RCTRootView *)self.window).loadingView = vc.view;
   
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
 }
 
 @end
