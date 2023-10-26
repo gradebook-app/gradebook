2aaa99fd332838baae995fadb19f2c2887193ec3
mahitmehta <mahit.py@gmail.com>
2023-10-26 15:37:36 -0400

    Overflow Fix

--- AppNavigator.tsx
+++ AppNavigator.tsx
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import { NavigationContainer, DarkTheme, NavigatorScreenParams } from "@react-navigation/native";
+import { NavigatorScreenParams } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import NavigatorScreen, { BottomTabParamList } from "./src/pages/NavigatorScreen";
 import AssignmentsScreen, { IAssignmentNavigationParams } from "./src/pages/AssignmentsScreen";
@@ -55,131 +55,130 @@ const AppNavigator = () => {
     const { theme } = useTheme();
 
     return (
-        <NavigationContainer theme={DarkTheme}>
-            <Stack.Navigator initialRouteName="loading">
-                <Stack.Screen 
-                    name="loading"
-                    component={LoadingScreen}
-                    options={{ headerShown: false, gestureEnabled: true }}
-                />
-                <Stack.Screen 
-                    name="login"
-                    component={LoginScreen}
-                    options={{ 
-                        headerShown: false, 
-                        gestureEnabled: false,
-                    }}
-                />
-                <Stack.Screen
-                    name="navigator"
-                    component={NavigatorScreen}
-                    options={{ headerShown: false, gestureEnabled: false }}
-                />
-                <Stack.Screen 
-                    name="gpa"
-                    component={GPAScreen}
-                    options={{
-                        headerShown: true, 
-                        headerTitle: "GPA Summary", 
-                        headerTitleStyle: {
-                            color: theme.text
-                        },
-                        gestureEnabled: true,
-                        headerBackTitle: "",
-                    }}
-                />
-                <Stack.Screen
-                    name="assignments"
-                    component={AssignmentsScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "",
-                    }}
-                />
-                <Stack.Screen
-                    name="notifications"
-                    component={NotificationScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "Options",
-                    }}
-                />
-                <Stack.Screen
-                    name="contact"
-                    component={ContactScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "Account",
-                    }}
-                />
-                <Stack.Screen
-                    name="security"
-                    component={SecurityScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "Options",
-                    }}
-                />
-                <Stack.Screen
-                    name="privacy-policy"
-                    component={PrivacyPolicyScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "Account",
-                    }}
-                />
-                <Stack.Screen
-                    name="options"
-                    component={OptionsScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "Account",
-                    }}
-                />
-                <Stack.Screen
-                    name="advanced-options"
-                    component={AdvancedOptionsScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "Options",
-                    }}
-                />
-                <Stack.Screen
-                    name="donate"
-                    component={DonateScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        headerBackTitle: "",
-                        gestureEnabled: true,
-                    }}
-                />
-                <Stack.Screen 
-                    name="ads-settings"
-                    component={AdsSettingsScreen}
-                    options={{ 
-                        headerShown: true, 
-                        headerTitle: "", 
-                        gestureEnabled: true,
-                        headerBackTitle: "Options",
-                    }}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+        
+        <Stack.Navigator initialRouteName="loading">
+            <Stack.Screen 
+                name="loading"
+                component={LoadingScreen}
+                options={{ headerShown: false, gestureEnabled: true }}
+            />
+            <Stack.Screen 
+                name="login"
+                component={LoginScreen}
+                options={{ 
+                    headerShown: false, 
+                    gestureEnabled: false,
+                }}
+            />
+            <Stack.Screen
+                name="navigator"
+                component={NavigatorScreen}
+                options={{ headerShown: false, gestureEnabled: false }}
+            />
+            <Stack.Screen 
+                name="gpa"
+                component={GPAScreen}
+                options={{
+                    headerShown: true, 
+                    headerTitle: "GPA Summary", 
+                    headerTitleStyle: {
+                        color: theme.text
+                    },
+                    gestureEnabled: true,
+                    headerBackTitle: "",
+                }}
+            />
+            <Stack.Screen
+                name="assignments"
+                component={AssignmentsScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "",
+                }}
+            />
+            <Stack.Screen
+                name="notifications"
+                component={NotificationScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "Options",
+                }}
+            />
+            <Stack.Screen
+                name="contact"
+                component={ContactScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "Account",
+                }}
+            />
+            <Stack.Screen
+                name="security"
+                component={SecurityScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "Options",
+                }}
+            />
+            <Stack.Screen
+                name="privacy-policy"
+                component={PrivacyPolicyScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "Account",
+                }}
+            />
+            <Stack.Screen
+                name="options"
+                component={OptionsScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "Account",
+                }}
+            />
+            <Stack.Screen
+                name="advanced-options"
+                component={AdvancedOptionsScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "Options",
+                }}
+            />
+            <Stack.Screen
+                name="donate"
+                component={DonateScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    headerBackTitle: "",
+                    gestureEnabled: true,
+                }}
+            />
+            <Stack.Screen 
+                name="ads-settings"
+                component={AdsSettingsScreen}
+                options={{ 
+                    headerShown: true, 
+                    headerTitle: "", 
+                    gestureEnabled: true,
+                    headerBackTitle: "Options",
+                }}
+            />
+        </Stack.Navigator>
     );
 };
 
